af56d8518f35bd2d567f67dbca01ef23c699aa7d
ashutosh <ashutoshsingh.iimt@gmail.com>
2021-10-16 14:06:06 +0530

    new file added

--- app/src/main/java/com/example/login_test/MainActivity.java
+++ app/src/main/java/com/example/login_test/MainActivity.java
@@ -7,8 +7,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-
 public class MainActivity extends AppCompatActivity {
 Button logout;
 SharedPreferences sp;
